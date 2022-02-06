a2a1e6330980397fbf611eab92b173c9edb41fe7
Ruslan <ruslrol17@gmail.com>
2022-02-06 20:33:46 +0500

    comit init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style/index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let counter = 0;
+
+export default function App() {
+    const [text, setText] = useState("")
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (event) => {
+        event.preventDefault();
+        if (!text) return;
+        const todo = { 
+            id: counter++, 
+            text, 
+            completed: false };
+            setText("");
+            setTodos([...todos, todo]);
+    }
+
+    const deleteItem = (item) => {
+        const filtered = todos.filter((todo) => todo !== item);
+        setTodos(filtered)
+    }
+
+    const deleteCompleted = () => {
+        const delCompleted = todos.filter((todo) => !todo.completed);
+        setTodos(delCompleted)
+    }
+
+    const toggle = (item) => {
+        const newTodos = todos.map((todo) => 
+            todo === item ? { ...todo, completed: !todo.completed } : todo,
+            );
+        setTodos(newTodos)
+    }
+
+    const deleteAll = todos.filter((item) => item.completed).length > 0;
+
+    return (
+            <div className="container">
+                <h1 className="title">ToDo List</h1>
+                <div className="header">
+                    <form onSubmit={addTodo}>
+                        <input className="input"
+                            type="text"
+                            placeholder="Напишите задачу..."
+                            value={text}
+                            onChange={(event) => setText(event.target.value)}
+                        />
+                        <button className="button" 
+                        disabled={!text}>Добавить</button>
+                    </form>
+                    <hr />
+
+                    <div className="block-list">
+                        <ul className="block-list-ul">
+                            {todos.map((todo) =>
+                                <li key={todo.id}>
+                                    <div className="list-active">
+                                        <input className="input-checkbox" 
+                                        type="checkbox" 
+                                        checked={todo.value} 
+                                        onChange={() => toggle(todo)} />
+                                        <label className="label-text">{todo.text}</label>
+                                        <button className="remove" type="button"
+                                            onClick={() => deleteItem(todo)}>
+                                            ❌
+                                        </button>
+                                    </div>
+                                </li>)}
+                        </ul>
+                    </div>
+
+                    <div className="block-delete">
+                        <button className="buttonLeft" 
+                        type="button"
+                        onClick={() => deleteCompleted()}
+                        disabled={!deleteAll}>
+                        Удалить завершенные
+                        </button>
+                        <button className="buttonRight" 
+                        type="button"
+                        onClick={() => setTodos([])}
+                        disabled={todos.length === 0}>
+                        Удалить все
+                        </button>   
+                    </div>
+
+                </div>
+            </div>
+    )
 }
-
-export default App;
